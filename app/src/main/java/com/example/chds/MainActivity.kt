d7442edc41eaf4f3c925865ce553337d126c08a7
AnthonyMarkD <44737462+AnthonyMarkD@users.noreply.github.com>
2021-03-18 14:20:34 -0600

    App added

---
+++ app/src/main/java/com/example/chds/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
