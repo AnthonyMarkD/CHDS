201f916e2304370cdb5aeba3eb8362f3515b273a
Hongru Qi <35446863+MichaelQi11@users.noreply.github.com>
2021-04-10 15:52:08 -0600

    Updated geofencing
    Updated geofencing

--- Android/app/src/main/java/com/example/chds/data/LocationBubble.kt
+++ Android/app/src/main/java/com/example/chds/data/LocationBubble.kt
@@ -2,13 +2,15 @@ package com.example.chds.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.versionedparcelable.ParcelField
 
 @Entity(tableName = "location_table")
-data class Location(
+data class LocationBubble(
     var locationName: String,
     var enabled: Boolean,
-    var lat: Long,
-    var lon: Long
+    var lat: Double,
+    var lon: Double,
+    var radius: Double
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
--- Android/app/src/main/java/com/example/chds/geofencing/LocationViewModel.kt
+++ Android/app/src/main/java/com/example/chds/geofencing/LocationViewModel.kt
@@ -5,16 +5,17 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.chds.data.Location
+import com.example.chds.data.LocationBubble
 import com.example.chds.database.LocationDatabase
 import com.example.chds.database.LocationRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class LocationViewModel(application: Application) : AndroidViewModel(application) {
-    val getAllLocations: LiveData<List<Location>>
-    val selectedLocation = MutableLiveData<Location>()
-
+    val getAllLocations: LiveData<List<LocationBubble>>
+    val selectedLocation = MutableLiveData<LocationBubble>()
+    val updatedLocation = MutableLiveData<LocationBubble>()
+    var update = false
 
     private val repository: LocationRepository
 
@@ -24,13 +25,28 @@ class LocationViewModel(application: Application) : AndroidViewModel(application
         getAllLocations = repository.readAllData
     }
 
-    fun setCurrentLocation(location: Location) {
-        selectedLocation.value = location
+    fun setCurrentLocation(locationBubble: LocationBubble) {
+        selectedLocation.value = locationBubble
     }
 
-    fun addLocation(location: Location) {
+    fun setUpdatedLocation(locationBubble: LocationBubble){
+        updatedLocation.value = locationBubble
+    }
+
+    fun addLocation(locationBubble: LocationBubble) {
         viewModelScope.launch(Dispatchers.IO) {
-            repository.addLocation(location)
+            repository.addLocation(locationBubble)
+        }
+    }
+
+    fun updateLocation(locationBubble: LocationBubble) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateLocation(locationBubble)
+        }
+    }
+    fun deleteLocation(locationBubble: LocationBubble) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteLocation(locationBubble)
         }
     }
 }
